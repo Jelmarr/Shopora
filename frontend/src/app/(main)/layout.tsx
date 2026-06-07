0451f24e4d6e60cb403b6c25a1b6f2235b745d3b
shyzenn <jelmarrapis47@gmail.com>
2026-06-07 15:36:35 +0800

    added email url validation in signup page

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sileo";
 import Sidebar from "@/src/components/Sidebar/Sidebar";
 
 export default function RootLayout({
@@ -8,6 +9,7 @@ export default function RootLayout({
   return (
     <section>
       <Sidebar />
+      <Toaster position="top-center" />
       <main className="flex justify-center mx-auto my-0">{children}</main>
     </section>
   );
